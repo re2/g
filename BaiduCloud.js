eb35c779cd432961ff759013999be2088f9324ca
任笑 <re2@users.noreply.github.com>
2021-03-05 11:00:58 +0800

    Update BaiduCloud.js

--- BaiduCloud.js
+++ BaiduCloud.js
@@ -5,7 +5,7 @@ Surge4.0:
 http-response https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/BaiduCloud.js
 
 QX1.0.0:
-https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/BaiduCloud.js
+https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user url script-response-body https://raw.githubusercontent.com/re2/g/master/BaiduCloud.js
 
 MITM = pan.baidu.com
 */
